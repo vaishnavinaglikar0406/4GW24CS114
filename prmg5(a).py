cda6a11ac0411a5fc1e75a20680b0e899f0673fd
Vaishnavi Naglikar <vaishnavinaglikar0406@gmail.com>
2026-01-17 16:38:58 +0530

    prmg5(a).py

---
+++ prmg5(a).py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+data = np.random.randn(1000)  
+distribution 
+plt.hist(data, bins=20, edgecolor='black', alpha=0.7) 
+plt.xlabel('Values') 
+plt.ylabel('Frequency') 
+plt.title('Histogram Plot Example') 
+plt.show()

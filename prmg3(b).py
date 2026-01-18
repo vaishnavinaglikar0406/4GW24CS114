e48fec03202d58419a6b1f7e7d3d7b65aa3fecac
Vaishnavi Naglikar <vaishnavinaglikar0406@gmail.com>
2026-01-18 20:02:02 +0530

    prmg3(b).py

--- prmg3(b).py
+++ prmg3(b).py
@@ -2,16 +2,19 @@ import difflib
 def string_similarity(str1, str2): 
   result =  difflib.SequenceMatcher(a=str1.lower(), b=str2.lower()) 
   return result.ratio() 
-str1 = 'Python Exercises' or str1 = input("Enter a sentence: ") 
-str2 = 'Python Exercises' or str2 = input("Enter a sentence: ") 
+str1 = 'Python Exercises' 
+str1 = input("Enter a sentence: ") 
+str2 = 'Python Exercises' 
+str2 = input("Enter a sentence: ") 
 print("Original string:") 
 print(str1) 
 print(str2) 
 print("Similarity between two said strings:") 
 print(string_similarity(str1,str2)) 
-str1 = 'Python Exercises' or str1 = input("Enter a sentence: ") 
+str1 = 'Python Exercises' 
+str1 = input("Enter a sentence: ") 
 str2 = 'Python Exercise' 
-or str2 = input("Enter a sentence: ") 
+str2 = input("Enter a sentence: ") 
 print("\nOriginal string:") 
 print(str1) 
 print(str2) 
